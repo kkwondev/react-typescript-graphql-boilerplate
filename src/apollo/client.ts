d69b840a9068e70f6755dff8d76aded06a17b1b3
kkwon <kkwoncokr@gmail.com>
2021-03-14 00:32:08 +0900

    fix : apollo .env setting

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,7 +2,7 @@ import {ApolloClient, createHttpLink} from '@apollo/client';
 import {cache} from './cache';
 
 const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_API_URL,
+    uri: process.env.REACT_APP_API_URI,
 })
 export const client = new ApolloClient({
     link: httpLink,
