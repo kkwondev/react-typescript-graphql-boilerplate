7398049d947d9562a3230354e406cf814890bc70
kkwon <kkwoncokr@gmail.com>
2021-03-14 23:34:34 +0900

    [Feat] req headers add

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,10 +1,21 @@
-import {ApolloClient, createHttpLink} from '@apollo/client';
+import {ApolloClient, ApolloLink, concat, createHttpLink} from '@apollo/client';
 import {cache} from './cache';
 
 const httpLink = createHttpLink({
     uri: process.env.REACT_APP_API_URI,
 })
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+    operation.setContext({
+      headers: {
+        Authorization: localStorage.getItem('Authorization') || null,
+      }
+    });
+  
+    return forward(operation);
+  })
 export const client = new ApolloClient({
-    link: httpLink,
+    link: concat(authMiddleware,httpLink),
     cache :cache,
 })
