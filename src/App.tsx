6b21d537d44fd4098bb689b397f2f1303a74c989
kkwon <kkwoncokr@gmail.com>
2021-03-14 23:50:52 +0900

    [Feat] Redirect setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {client} from "./apollo/client"
 import { ApolloProvider } from '@apollo/client';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import MyPage from './components/pages/MyPage';
 import SigninPage from './components/pages/SigninPage';
 import SignupPage from './components/pages/SignupPage';
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" component={MyPage} exact={true}/>
         <Route path="/signin" component={SigninPage}/>
         <Route path="/signup" component={SignupPage}/>
+        <Redirect path="*" to="/" />
       </Switch>
     </ApolloProvider>
   );
